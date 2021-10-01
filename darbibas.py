b249fb93adb85e66288d4e0bee609e6e483c4086
Zokkers <66552801+Zokkers@users.noreply.github.com>
2021-10-01 10:19:54 +0300

    Update darbibas.py

---
+++ darbibas.py
@@ -0,0 +1,21 @@
+skaitlis1=4
+skaitlis2=3
+
+summa = skaitlis1 + skaitlis2 #summa
+print(summa)
+print(skaitlis1 , "+" , skaitlis2 , "=" , summa)
+
+starpiba = skaitlis1 - skaitlis2 #starpība
+print(starpiba)
+
+reizinajums = skaitlis1 * skaitlis2 #reizinājums
+print(reizinajums)
+
+dalijums = skaitlis1 / skaitlis2 #dalījums
+print(dalijums)
+
+dalijums2 = skaitlis1 // skaitlis2 #dalījums bez atlikuma
+print(dalijums2)
+
+atlikums = skaitlis1 % skaitlis2 #atlikuma notiekšana
+print(atlikums)
