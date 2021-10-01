3bfbd624a4d535691864fcad6cd89e2e65b5e86e
Zokkers <66552801+Zokkers@users.noreply.github.com>
2021-10-01 10:05:04 +0300

    darbibas

--- programma1.py
+++ programma1.py
@@ -1,8 +1,20 @@
 #print("Hello, World!")
 print("Ivo")
 
-
 skaitlis=5
 vardi="Skola"
 print(skaitlis)
 print(vardi)
+
+v1, v2, v3 = "Saule", "Vējš", "Ūdens"
+print(v1 , v2 , v3) #3 mainīgie 1 salikumā
+
+m1 = m2 = m3 = "Viens simbols"
+print(m1 , m2 , m3) #Pārvēršana 3 mainīgos par 1 izvadi
+
+def nosaukumi(name):
+    print("Sveiki" , name)
+
+nosaukumi("Sveicināti")
+nosaukumi("Liepa")
+nosaukumi("Ozols")#Funkciju definēšana
